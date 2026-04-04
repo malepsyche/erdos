4da2ced216dc8cafe18efe955cdfc01ffa48a166
malepsyche <114489469+malepsyche@users.noreply.github.com>
2026-04-04 13:52:26 +0800

    Update queue.h

--- fountain/common/queue.h
+++ fountain/common/queue.h
@@ -30,6 +30,43 @@ class ConcurrentBoundedQueue {
     full__cond_.notify_one();
   }
 
+  void push(T& item) {
+    std::unique_lock<std::mutex> mlock(mutex_);
+    while (!killed_ && queue_.size() >= bound) {
+      full_cond_.wait(mlock);
+    }
+    if (killed_) {
+      return;
+    }
+    queue_.push(std::move(item));
+    ++input_count_;
+    mlock.unlock();
+    empty_cond_.notify_one();
+  }
+
+  size_t size() {
+    std::unique_lock<std::mutex> mlock(mutex_);
+    if (killed_) {
+      return 0;
+    }
+    return queue_.size();
+  }
+
+  size_t input_count() { return input_count_; }
+
+  void kill(std::function<T()> empty_creator) {
+    std::unique_lock<std::mutex> mlock(mutex_);
+    if (killed_) {
+      return;
+    }
+    killed_ = true;
+    empty_creator_ = empty_creator;
+    empty_cond_.notify_all();
+    full_cond_.notify_all();
+  }
+
+  /* ======== */
+
  private:
   size_t bound_;
   size_t input_count_ = 0;
