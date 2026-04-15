57ce0d8e0ef9e4e8cd39837b271af402a29a6187
malepsyche <114489469+malepsyche@users.noreply.github.com>
2026-04-15 15:40:22 +0800

    Create ring_buffer.h

---
+++ spring/common/ring_buffer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <cstddef> 
+
+namespace euclid {
+namespace spring {
+
+template <typename T, std::size_t Capacity>
+class SPSCRingBuffer {
+  static_assert(Capacity >= 2, "Capacity must be at least 2");
+  static_assert((Capacity & (Capacity-1)) == 0, "Capacity must be a power of 2 for bitmasking");
+
+  public:
+    SPSCRingBuffer() = default;
+    ~SPSCRingBuffer() {};
+
+  private:
+    static constexpr std::size_t increment(std::size_t i) noexcept {
+      return (i+1) & mask_;
+    }
+
+  private:
+    static constexpr std::size_t mask_ = Capacity - 1;
+    static constexpr std::size_t cacheline_size_ = 64;
+
+    alignas(T) std::byte storage_[Capacity][sizeof(T)];
+    alignas(cacheline_size_) std::atomic<std::size_t> head{0};
+    alignas(cacheline_size_) std::atomic<std::size_t> tail{0};
+};
+
+} // namespace spring
+} // namespace euclid
