d48d7f9fe267cf96f78221a2ede4f9cd7f83bf9c
malepsyche <114489469+malepsyche@users.noreply.github.com>
2026-04-28 19:37:10 +0800

    Create log_event.hpp

---
+++ spring/common/log_event.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <cstdint>
+
+#include "spring/market/market_event.hpp"
+
+namespace euclid {
+namespace spring {
+
+enum class LogStage : std::uint8_t {
+  MarketGenerated = 0
+};
+
+struct LogEvent {
+  std::uint64_t log_ts_ns;
+  std::uint64_t seq_no;
+  std::uint16_t producer_id;
+  LogStage stage;
+
+  union {
+    MarketEventLog market_event_log;
+  };
+};
+
+} // namespace spring
+} // namespace euclid
